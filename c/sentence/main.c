3c2f9f699a5d326df4c678b8d20b2bb3fbb9cb34
RStyle <rsty96@googlemail.com>
2017-11-27 09:34:38 +0100

    Update main.c

--- c/sentence/main.c
+++ c/sentence/main.c
@@ -23,12 +23,14 @@ int main(void) {
 	insertWordAtPosition(&sentence1, "error", 8);
 	insertWordAtPosition(&sentence1, "error", -8);
 	printSentence(sentence1);
+	// 1	2	3
 	insertWordAtPosition(&sentence1, "4", 3);
 	insertWordAtPosition(&sentence1, "3.2", 3);
 	insertWordAtPosition(&sentence1, "0", 0);
 	insertWordAtPosition(&sentence1, "0.2", 1);
 	insertWordAtEnd(&sentence1, "End");
 	printSentence(sentence1);
+	// 0	0.2	1	2	3	3.2	4	End
 
 	printf("delete at front: ");
 	deleteWordAtFront(&sentence1, deletedWord);
@@ -37,23 +39,29 @@ int main(void) {
 	deleteWordAtEnd(&sentence1, deletedWord);
 	printf("deleted: '%s'\n", deletedWord);
 	printSentence(sentence1);
+	// 0.2	1	2	3	3.2	4
 
 	char ch[][16] = {"C", "is", "fun"};
 	struct sentence sentence2 = createSentenceFromArray(ch, 3);
 	printf("createSentenceFromArray test: ");
 	printSentence(sentence2);
+	// C	is	fun
 	deleteWordAtPosition(&sentence2, deletedWord, 1);
 	printf("deleted at pos[1]: '%s'\n", deletedWord);
 	printSentence(sentence2);
+	// C	fun
 	deleteWordAtPosition(&sentence2, deletedWord, 1);
 	printf("deleted at pos[1]: '%s'\n", deletedWord);
 	printSentence(sentence2);
+	// C
 	deleteWordAtPosition(&sentence2, deletedWord, 1);
 	printf("deleted at pos[1]: '%s'\n", deletedWord);
 	printSentence(sentence2);
+	// C
 	printf("deleteSentence and then print test: ");
 	deleteSentence(&sentence1);
 	printSentence(sentence1);
+	// Print Error: empty list
 
 	return 0;
 }
