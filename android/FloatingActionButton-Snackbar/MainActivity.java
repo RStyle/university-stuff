61ed85f48a16b5aed1f8a0d2f4d28c1ac9bf131b
RStyle <rsty96@googlemail.com>
2017-12-04 15:22:44 +0100

    Create MainActivity.java

---
+++ android/FloatingActionButton-Snackbar/MainActivity.java
@@ -0,0 +1,21 @@
+
+    ...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Custom Message", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    
+    ...
